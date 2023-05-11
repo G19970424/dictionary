46718cd3a7fb5a068e0dc782addbf229a9948ac6
gejj <1015636737@qq.com>
2023-05-11 17:48:38 +0800

    excel utils

--- dictionary-common/src/main/java/cn/com/dictionary/common/exception/ExcelException.java
+++ dictionary-common/src/main/java/cn/com/dictionary/common/exception/ExcelException.java
@@ -1,8 +1,20 @@
 package cn.com.dictionary.common.exception;
 
+import cn.com.dictionary.common.GlobalException;
+
 /**
  * @author gejj
  * @data 2023/5/8 16:16
+ * Global Excel Exception
  */
-public class ExcelException extends GlobalException{
+public class ExcelException extends GlobalException {
+
+
+    public ExcelException(){
+        super();
+    }
+
+    public ExcelException(String message){
+        super(message);
+    }
 }
